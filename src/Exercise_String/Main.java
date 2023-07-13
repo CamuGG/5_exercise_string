001e7b05fbf7e8781eb26a1864f422672c657940
Camu <simone.benedetti94@gmail.com>
2023-07-13 22:07:58 +0200

    add exercise to string

---
+++ src/Exercise_String/Main.java
@@ -0,0 +1,86 @@
+package Exercise_String;
+
+public class Main {
+    public static void main(String[] args) {
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4();
+    }
+
+    /**
+     * 1: Create a String to hold some text, decide on the text you want it to hold and then name it appropriately
+     *
+     *    print out your string in upper case, and in lower case
+     */
+    private static void exercise1() {
+        System.out.println("\nExercise 1:");
+        //Write your code here
+        String myName = "Simone";
+        System.out.println(myName.toUpperCase());
+        System.out.println(myName.toLowerCase());
+    }
+
+    /**
+     * 2: Edit the code below so it prints out the first name and last name in uppercase, and the middle name in lower case
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2:");
+        // Change these
+        String firstName = "Franco";
+        String middleName = "Antonio";
+        String lastName = "Beccaria";
+
+        System.out.println(firstName.toUpperCase() + " " + middleName.toLowerCase() + " " + lastName.toUpperCase()); // <--- Edit this line
+    }
+
+
+     /**
+     * 3: Assign the value of 2 or more primitive types to our variable textFrom2Primitives
+     *
+     * Use the '+' operator
+     *
+     * choose from these primitives: double, int, char, boolean
+     * use primitive literals like: 1.5, 67, 'b', true
+     *
+     * +
+     *
+     * Experiment with these different types, how do an int and a double behave when added?
+     *
+     * What about a boolean and an int?
+      *
+      * "" + 1.5
+     *
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3:");
+
+        String textFrom2Primitives = "true" + "5";// <--- Edit this line
+        String textFrom2Primitives2 = "5.5" + "2";
+
+        System.out.println(textFrom2Primitives);// ci aspettiamo:  true5
+        System.out.println(textFrom2Primitives2);// ci aspettiamo: 5.52
+
+        // i valori all'interno delle stringhe non sono soggetti alle normali operazioni matematiche,
+        // perchè il valore di una stringa è costituito da un'unione di caratteri
+    }
+
+
+     /**
+     * 4: Without editing the Strings below make the print statement print
+     *
+     *    1 2 3 4 5 6 7 8 9 with the correct spacing in between all the numbers
+     *
+     *    You will need to use .trim()
+     */
+    private static void exercise4() {
+        System.out.println("\nExercise 4:");
+        String endNumbers = "7 8 9";
+        String middleNumbers = "       4 5 6";
+        String earlyNumbers = "1 2 3";
+
+        System.out.println(earlyNumbers + " " + middleNumbers.trim() + " " + endNumbers); // <--- Write your code here
+    }
+
+
+}
